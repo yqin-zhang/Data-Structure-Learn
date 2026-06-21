9cea0fb807df8c712ed94e5f28d870a46f08dd16
yqin-zhang <2260540909@qq.com>
2026-06-21 17:01:06 +0800

    另一颗子树

---
+++ 另一颗子树/另一颗子树/test.cpp
@@ -0,0 +1,61 @@
+#define  _CRT_SECURE_NO_WARNINGS    1	
+#include<iostream>
+using namespace std;
+
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+        {
+            return true;
+        }
+        if (p == NULL || q == NULL)
+        {
+            return false;
+        }
+        if (p->val != q->val)
+        {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == NULL)
+        {
+            return false;
+        }
+        if (isSameTree(root, subRoot))
+        {
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot)|| isSubtree(root->right, subRoot);
+    }
+};
+
+int main()
+{
+
+    TreeNode* p = new TreeNode(1);
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(3);
+
+    TreeNode* q = new TreeNode(1);
+    q->left = new TreeNode(2);
+    q->right = new TreeNode(3);
+
+    Solution s;
+    s.isSubtree(q, p);
+    return 0;
+}
