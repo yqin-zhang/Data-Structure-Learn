27c3fac4be8ccd643862592e3952d089edd306c8
yqin-zhang <2260540909@qq.com>
2026-06-21 17:01:43 +0800

    相同的树

---
+++ 相同的树/相同的树/test.cpp
@@ -0,0 +1,48 @@
+#define  _CRT_SECURE_NO_WARNINGS    1	
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == NULL && q == NULL)
+        {
+            return true;
+        }
+        if (p == NULL || q == NULL)
+        {
+            return false;
+        }
+        if (p->val != q->val)
+        {
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+int main()
+{
+
+    TreeNode* p = new TreeNode(1);
+    p->left = new TreeNode(2);
+    p->right = new TreeNode(3);
+
+    TreeNode* q = new TreeNode(1);
+    q->left = new TreeNode(2);
+    q->right = new TreeNode(3);
+
+    Solution s;
+    s.isSameTree(q, p);
+	return 0;
+}
